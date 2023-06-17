ecacb82c38b36d8289cc0a87a9c17b162b621d72
Gabriel Garcia Jaubert <ggjaubert@gmail.com>
2023-06-17 16:55:37 +0200

    first commit

---
+++ src/MyBlockchain.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MyBlockchain.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Worasdasd!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
